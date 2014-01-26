d41535a16e8f5f292d0eaebbe3d6b72e2e3a261f
Peter Pilgrim <peter.pilgrim@gmail.com>
2014-01-26 18:54:31 +0000

    Added edit project track. Because of a bug in Mojarra 2.2.0 in the GlassFish 4.0 release the f:viewAction does not work as documented in JSF 2.2 with new xmlns JCP namespace. So I used the <f:event type="preRenderView" listener="#{catalog.checkItem}"/> instead.

--- ch01/xentracker-basic/src/main/java/je7hb/intro/xentracker/control/ProjectEditController.java
+++ ch01/xentracker-basic/src/main/java/je7hb/intro/xentracker/control/ProjectEditController.java
@@ -3,7 +3,7 @@ package je7hb.intro.xentracker.control;
 import je7hb.intro.xentracker.boundary.ProjectTaskService;
 import je7hb.intro.xentracker.entity.Project;
 
-import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
@@ -18,9 +18,9 @@ import java.util.List;
  * @author Peter Pilgrim
  */
 
-@ManagedBean(name="projectViewController")
-@SessionScoped
-public class ProjectViewController {
+@ManagedBean(name="projectEditController")
+@ViewScoped
+public class ProjectEditController {
 
     @Inject ProjectTaskService service;
 
@@ -29,33 +29,25 @@ public class ProjectViewController {
     private String headline;
     private String description;
 
-    public List<Project> getProjectList() {
-        List<Project> projects = service.findAllProjects();
-        return projects;
-    }
+    public void findProjectById() {
+        System.out.printf("===>>> %s.init() called id=%d\n", this.getClass().getSimpleName(), id );
+        if (id <= 0) {
+            String message = "Bad request. Please use a link from within the system.";
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
+            return;
+        }
 
-    public void chooseBookFromLink(ActionEvent event) {
-        String current = event.getComponent().getId();
-        FacesContext context = FacesContext.getCurrentInstance();
-//        String bookId = books.get(current);
-//        context.getExternalContext().getSessionMap().put("bookId", bookId);
-    }
+        Project project = service.findProjectById(id).get(0);
 
-    // Unable to find matching navigation case with from-view-id '/index.xhtml' for action '#{projectViewController.createNewProject}' with outcome 'create-project'
-    public String navigateNewProject() {
-        System.out.printf("%s.navigateNewProject() called\n", this.getClass().getSimpleName());
-        return "createProject?faces-redirect=true";
-    }
-
-    // Unable to find matching navigation case with from-view-id '/index.xhtml' for action '#{projectViewController.createNewProject}' with outcome 'create-project'
-    public String createNewProject() {
-        System.out.printf("%s.createNewProject() called\n", this.getClass().getSimpleName());
-        Project project = new Project(name, headline, description);
-        service.saveProject(project);
-        name = null;
-        headline = null;
-        description = null;
-        return "index?faces-redirect=true";
+        if (project == null) {
+            String message = "Bad request. Unknown user.";
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
+        }
+        name = project.getName();
+        headline = project.getHeadline();
+        description = project.getDescription();
     }
 
     public String cancel() {
@@ -63,19 +55,33 @@ public class ProjectViewController {
         return "index?faces-redirect=true";
     }
 
-    public String editProject(int projectId) {
-        System.out.printf("%s.editProject(projectId=%d) called\n", this.getClass().getSimpleName(), projectId );
+    public String viewEdit(int projectId) {
+        System.out.printf("===>>> %s.viewEdit( projectId=%d ) called\n", this.getClass().getSimpleName(), projectId);
 
         Project project = service.findProjectById(projectId).get(0);
         id = project.getId();
         name = project.getName();
         headline = project.getHeadline();
         description = project.getDescription();
+        System.out.printf("===>>> project = %s\n", project);
 
         String contextPath = FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath();
-        System.out.printf("contextPath=%s\n", contextPath);
 
         return contextPath +"/views/editProject.xhtml";
+    }
+
+    public String saveEdit() {
+        System.out.printf("===>>> %s.saveEdit() called\n", this.getClass().getSimpleName());
+
+        Project project = service.findProjectById(id).get(0);
+        project.setName(name);
+        project.setHeadline(headline);
+        project.setDescription(description);
+
+        service.updateProject(project);
+        System.out.printf("===>>> project = %s\n", project);
+
+        return "index?faces-redirect=true";
     }
 
     public int getId() {
